74fc9edb4e13d9368439c40f35d4ab9ab5122b7c
Cristobal Maldonado <cristobal.maldonado.a@gmail.com>
2021-11-04 23:42:40 -0300

    Remove unused imports

--- src/pages/Reviewer/Tenders/index.js
+++ src/pages/Reviewer/Tenders/index.js
@@ -1,6 +1,4 @@
-import { Col, Row } from "antd";
 import List from "common/display/List";
-import { DeleteAction, EditAction } from "common/Form/ActionButton";
 import Layout from "common/Layout";
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router";
@@ -8,28 +6,6 @@ import { useLocation } from "react-router";
 export default function Tenders({ ...props }) {
   const { state } = useLocation();
   const [tenders, setTenders] = useState([]);
-
-  const deleteTender = (id) =>
-    fetch(`${process.env.REACT_APP_API}/tenders/${id}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: sessionStorage.getItem("auth"),
-      },
-    })
-      .then((response) => response.json())
-      .then((success) =>
-        fetch(`${process.env.REACT_APP_API}/tenders`, {
-          method: "GET",
-          headers: {
-            Authorization: sessionStorage.getItem("auth"),
-          },
-        })
-      )
-      .then((response) => response.json())
-      .then((success) => {
-        setTenders(success.tenders);
-      })
-      .catch((error) => console.log(error));
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API}/tenders`, {
